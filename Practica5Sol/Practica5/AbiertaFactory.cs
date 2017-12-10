0cf506fea320f932a12fb61f7111092e99d47638
Pablo Sánchez <sanchezbp@unican.es>
2017-12-10 19:19:06 +0100

    Corrige la creación de objetos mediante patrón Prototipo
    En el patrón prototipo, siempre que se crea un objeto nuevo a partir del prototipo, hay que clonar la instancia que sirve de prototipo, ya que cada instancia clonada podría ser luego configurada de manera diferente. Por tanto, siempre que se crea una objeto nuevo a partir del prototipo, hay que hacer prototipo.Clone(). Si es Java, no hay problema, en C# hay que heredar de la interfaz Cloneable e implementar el método que corresponda.

--- Practica5Sol/Practica5/AbiertaFactory.cs
+++ Practica5Sol/Practica5/AbiertaFactory.cs
@@ -9,8 +9,13 @@ namespace Practica5
     public class AbiertaFactory : ImpresoraFactory
     {
         protected AbiertaFactory() { }
+        
         public override Impresora createImpresora()
         {
+            // Aquí debería ser algo como protStr.Clone().
+            // La instancia prototípica hay que clonarla.
+            // Además la propiedad o atributo protoStr debería tenerla esta
+            // clase, que es donde se usa, y no la superclase.
             return new ImpresoraExtendida(protoStr);
         }
 
