28ace2d48c414bb75348c88fe706dd72802438b4
Pablo Sánchez <sanchezbp@unican.es>
2017-12-10 19:36:17 +0100

    Interfaz de observación
    Aquí hay algunos temas que me preocupan, principalmente porque reflejan problemas conceptuales con el patrón. Las interfaces de observación sólo deben contener métodos a través de los cuales se notifiquen cambios en las clases observadas.

--- Pr-06-Observer/EltoSistObserver.cs
+++ Pr-06-Observer/EltoSistObserver.cs
@@ -9,7 +9,16 @@ namespace Pr_06_Observer
 {
     public interface EltoSistObserver
     {
+        
+        // Esta propiedad no la entiendo, no sé muy bie para que sirve. 
+        // Nomalmente a los observadores no se les exige nada, más allá
+        // de que tengan un método donde se puedan notificar los cambios 
+        // de las clases observadas.
         String Nombre { get; set; }
+        // Esta interfaz vale sólo para los enlaces pero los controles
+        // necesitan observar cualquier elemento del sistema, por lo 
+        // que habría que crear otra interfaz que permita pasar Enlaces
+        // como parámetro.
         void updateEnlace(Enlazable elto);
     }
 }
